fbb0c99bbda94854fcd988665a73f5f5c83b0da6
milad <miladnorozii33@gmail.com>
2019-12-09 23:03:31 +0330

    initial commit

---
+++ information-crawler.py
@@ -0,0 +1,22 @@
+import urllib2
+from bs4 import BeautifulSoup
+import pyttsx
+import sys
+
+class Wikipedia:
+	def crawlSimpleParagraph(self,url):
+		req = urllib2.Request(url,headers={'User-Agent' : "Magic Browser"})
+		con = urllib2.urlopen(req)
+		soup = BeautifulSoup(con,'lxml')
+		div = soup.find("div",{"class":"mw-parser-output"})
+		for row in div.findAll('p'):
+			if row.getText():
+				print row.getText()
+
+engine = pyttsx.init()
+engine.setProperty("rate",150)
+voices = engine.getProperty('voices')
+wiki = Wikipedia()
+word = sys.argv[1]
+#wiki.crawlSimpleParagraph("https://en.wikipedia.org/wiki/Dennis_Ritchie")
+wiki.crawlSimpleParagraph("https://en.wikipedia.org/wiki/"+word)
