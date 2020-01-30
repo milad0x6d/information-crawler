901062bf40edee9dd025db9d6c5c4291d1925d08
milad <miladnorozii33@gmail.com>
2020-01-30 12:46:33 +0330

    word processing done

--- information-crawler.py
+++ information-crawler.py
@@ -12,12 +12,20 @@ class Wikipedia:
 		for row in div.findAll('p'):
 			print row.getText()
 			engine.say(row.getText())
-		engine.runAndWait()
+	engine.runAndWait()
 
 engine = pyttsx.init()
 engine.setProperty("rate",150)
 voices = engine.getProperty('voices')
 wiki = Wikipedia()
-word = sys.argv[1]
+stdin = ""
+for item in sys.argv[1:]:
+    if item != sys.argv[-1]: 
+        item = item[0:1].upper() + item[1:] + "_"
+        stdin += item
+    else:
+        item = item[0:1].upper() + item[1:]
+        stdin += item
+print stdin
 #wiki.crawlSimpleParagraph("https://en.wikipedia.org/wiki/Dennis_Ritchie")
-wiki.crawlSimpleParagraph("https://en.wikipedia.org/wiki/"+word)
+wiki.crawlSimpleParagraph("https://en.wikipedia.org/wiki/"+stdin)
